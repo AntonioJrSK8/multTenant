a6ca39f4ae21cf06c757a25fa085000e2f5215dc
AntonioJRSK8 <jr.sistema@gmail.com>
2021-11-20 08:07:57 -0300

    Criação do primeiro livewire Componente tenant

---
+++ app/Http/Livewire/Tenant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Tenant extends Component
+{
+    public function render()
+    {
+        return view('livewire.tenant');
+    }
+}
